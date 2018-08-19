cc3387109d9aef4d544739514c0970d9104d0bc1
Kateřina Paulinová <paulinova@movisio.com>
2018-08-19 22:38:54 +0200

    homepage, footer, navbar

--- homepage.php
+++ homepage.php
@@ -14,61 +14,327 @@
     <title>Pehapkari.cz</title>
 </head>
 <body>
+<nav class="container navbar-expand-lg navbar-light bg-light nav-ph">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+    <div class="nav-ph__logo">
+        <img src="src/icons/logo.svg">
+        <a href="#" class="ml-2">Pehápkáři</a>
+    </div>
+        <div class="nav-ph__menu">
+            <ul>
+                <li>
+                    <a href="#meetups">
+                        Meetups
+                    </a>
+                </li>
+                <li>
+                    <a href="#courses">
+                        Courses
+                    </a>
+                </li>
+                <li>
+                    <a href="#livestreams">
+                        Livestreams
+                    </a>
+                </li>
+                <li>
+                    <a href="#blog">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="#about">
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a href="#contact">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 
-<div class="homepage container">
-    <section>
-        <div class="heading">
-            <div class="row">
-                <div class="col-7">
-                    <div class="heading--title">
-                        <h1>
-                            The biggest Czech <br> and Slovak PHP community
-                        </h1>
-                    </div>
-                    <div class="heading--subtitle">
-                        <p>We are a group of over 2000 people interested in PHP, active on Facebook, Twitter, Slack, YouTube and Twitch.</p>
-                    </div>
-                    <div class="button button-primary">
-                        see what we do
+</nav>
+
+<div class="homepage">
+    <div class="container">
+        <section>
+            <div class="heading">
+                <div class="row">
+                    <div class="col-7">
+                        <div class="heading--title">
+                            <h1>
+                                The biggest Czech <br> and Slovak PHP community
+                            </h1>
+                        </div>
+                        <div class="heading--subtitle">
+                            <p>We are a group of over 2000 people interested in PHP, active on Facebook, Twitter, Slack, YouTube and Twitch.</p>
+                        </div>
+                        <div class="button button-primary">
+                            see what we do
+                        </div>
                     </div>
                 </div>
+                <div class="heading--background">
+                    <img src="src/images/homepage/1@2x.jpg" />
+                </div>
             </div>
-            <div class="heading--background">
-                <img src="src/images/homepage/1@2x.jpg" />
-            </div>
-        </div>
-    </section>
-    <section>
+        </section>
+    </div>
+    <section class="container-ph">
         <div class="row">
-            <div class="col-sm-7">
-                <div class="button button-secondary">
+            <div class="col-sm-5 flex-column d-flex">
+                <div class="par">
+                    <h2 class="par__title">
+                        Attend a meetup
+                    </h2>
+                    <p class="par__body">
+                        Everybody knows that our Slack community rocks. PHPStorm, Nette, Docker, Doctrine, Drupal, you name it, we’ve got it.
+                    </p>
+                </div>
+                <div class="button button-secondary mt-auto">
                     see all 3 meetups
                     <span><img src="src/icons/arrow_right_dark.svg"></span>
                 </div>
             </div>
 
-            <div class="col">
-                <div class="card-calendar" >
-                    <img class="card-calendar__card-img-top" src="src/images/courses/1.jpg" alt="Card image cap">
-                    <div class="card-calendar__card-body">
-                        <h6 class="card-calendar__card-body--card-title">PHP Pivo 2018/4 <br> - Pardubice</h6>
-                        <div class="card-calendar__card-body--address">
-                            KoPlac, Příkop 2, 60200 Brno
-                            <img src="src/icons/arro-upright.svg">
+            <div class="d-flex">
+                <div class="col-6">
+                    <div class="card-ph card-calendar" >
+                        <img class="card-calendar__card-img-top" src="src/images/homepage/1.jpg" alt="Card image cap">
+                        <div class="card-calendar__card-body">
+                            <h6 class="card-calendar__card-body--card-title">PHP Pivo 2018/4 <br> - Pardubice</h6>
+                            <div class="card-calendar__card-body--address">
+                                KoPlac, Příkop 2, 60200 Brno
+                                <img src="src/icons/arrow-upright.svg">
+                            </div>
+                            <div class="card-ph__card-body--date">
+                                <span class="card-ph__card-body--date-day">4</span>
+                                <span class="card-ph__card-body--date-month">APR</span>
+                            </div>
                         </div>
-                        <div class="card-calendar__card-body--date">
-                            <span class="card-calendar__card-body--date-day">4</span>
-                            <span class="card-calendar__card-body--date-month">APR</span>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card-ph card-calendar" >
+                        <img class="card-calendar__card-img-top" src="src/images/homepage/1.jpg" alt="Card image cap">
+                        <div class="card-calendar__card-body">
+                            <h6 class="card-calendar__card-body--card-title">2. zraz priateľov PHP v Bratislave - Pixel Federation</h6>
+                            <div class="card-calendar__card-body--address">
+                                Einsteinova 19, 851 01 Bratislava
+                                <img src="src/icons/arrow-upright.svg">
+                            </div>
+                            <div class="card-ph__card-body--date">
+                                <span class="card-ph__card-body--date-day">4</span>
+                                <span class="card-ph__card-body--date-month">APR</span>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         </div>
-
+    </section>
+    <section class="container">
+        <div class="row" style="height: 320px;">
+            <div class="col-sm-6 d-flex align-items-center">
+                <div class="background-banner"></div>
+                <div class="counter">
+                    <h2 class="counter__number">600 +</h2>
+                    <p class="counter__body">
+                        members active weekly
+                    </p>
+                </div>
+                <div class="counter">
+                    <h2 class="counter__number">55 +</h2>
+                    <p class="counter__body">
+                        channels, including #beer
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 d-flex flex-column">
+                <div class="mx-auto">
+                    <div class="par">
+                        <h2 class="par__title">
+                            Join our Slack
+                        </h2>
+                        <p class="par__body">
+                            Everybody knows that our Slack community rocks. PHPStorm, Nette, Docker, Doctrine, Drupal, you name it, we’ve got it.
+                        </p>
+                    </div>
+                    <div class="mt-5">
+                        <div class="button button-secondary">
+                            Invite yourself
+                            <span><img src="src/icons/arrow_right_dark.svg"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="par">
+                    <h2 class="par__title">
+                        Watch a <br> livestream
+                    </h2>
+                    <p class="par__body">
+                        Everybody knows that our Slack community rocks.
+                    </p>
+                </div>
+                <div class="button button-secondary" style="margin-top: 70px">
+                    More on livestreams
+                    <span><img src="src/icons/arrow_right_dark.svg"></span>
+                </div>
+            </div>
+            <div class="col-sm-6 align-self-end">
+                <div class="card-ph card-livestream">
+                    <div class="card-ph__card-body d-flex">
+                        <div class="w-25">
+                            <div class="card-ph__card-body--date">
+                                <span class="card-ph__card-body--date-day">4</span>
+                                <span class="card-ph__card-body--date-month">APR</span>
+                            </div>
+                        </div>
+                        <div class="d-block">
+                            <h3 class="card-ph__card-body--card-title">Postman - (Jakub Kratina)</h3>
+                            <p>19:00 - 21:00</p>
+                            <div class="card-ph__card-body-link">
+                                <a href="#">
+                                    Go to facebook event detail
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container">
+        <h2>Follow us on social media</h2>
+        <div class="row icon-row">
+            <div class="col-6 col-lg-3">
+                <div class="icon-social">
+                    <img src="src/icons/social/Facebook_White.svg"/>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3">
+                <div class="icon-social">
+                    <img src="src/icons/social/Twitter_White.svg"/>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3">
+                <div class="icon-social">
+                    <img src="src/icons/twitch_White.svg"/>
+                </div>
+            </div>
+            <div class="col-6 col-lg-3">
+                <div class="icon-social">
+                    <img src="src/icons/social/Youtube Copy_White.svg"/>
+                </div>
+            </div>
+        </div>
     </section>
 </div>
-
+<div class="footer-ph">
+    <div class="container">
+        <div class="footer-ph__header">
+            <div class="float-left mr-auto d-flex align-items-center">
+                <img src="/src/icons/logo.svg">
+                <span>Pehápkáři</span>
+            </div>
+            <div class="float-right">
+                <ul>
+                    <li>
+                        <a href="#"><img src="src/icons/social/Facebook_White.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="src/icons/social/Youtube Copy_White.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="src/icons/social/Slack_White.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="src/icons/twitch_White.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"> <img src="src/icons/social/Twitter_White.svg"></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="src/icons/social/Facebook_White.svg"></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-ph__body">
+            <div class="footer-ph__body--lef-column float-left mr-auto">
+                <p>Web Péhápkaři.cz pohání Statie a najdeš ho na Githubu.</p>
+                <p class="mt-4">Našel jsi chybu? Oprav nám ji prosím.</p>
+            </div>
+            <div class="float-right footer-ph__body--menu">
+                <div>
+                    <ul>
+                        <li class="footer-ph__body--menu-title">
+                            Companies
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">Sponsor us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li class="footer-ph__body--menu-title">
+                            Activities
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">Courses</a>
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">Meetups</a>
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">Livestreams</a>
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">Blog</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li class="footer-ph__body--menu-title">
+                            About
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">People</a>
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">Contact</a>
+                        </li>
+                        <li class="footer-ph__body--menu-subtitle">
+                            <a href="#">Download logo</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-ph__footer d-flex">
+            <div class="w-50">
+                <p>Friends:</p>
+            </div>
+            <div class="">
+                <img src="src/icons/jetbrains-blackandwhite.svg">
+                <img src="src/icons/posledni sobota.svg">
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
